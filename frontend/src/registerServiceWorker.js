10ad033acd10ec9b49c60c3b019f87b5a3876498
Lisa <lisa@plan8.se>
2020-11-01 12:12:29 +0100

    Update registerServiceWorker.js

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -107,7 +107,7 @@ function checkValidServiceWorker(swUrl) {
       );
     });
 }
-
+ 
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
